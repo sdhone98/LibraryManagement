ed956ebc2a63c78a161f9ccb9d3182cd27394dbe
sdhone98 <sdhone98@gmail.com>
2023-12-09 22:11:20 +0530

    First Commit - Sagar

---
+++ src/main/kotlin/com/sagardhone/library_management/LibraryManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.sagardhone.library_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/sagardhone/library_management/LibraryManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sagardhone.library_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
