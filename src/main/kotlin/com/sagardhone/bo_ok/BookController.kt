f3d149a796b1a0919fa8951eedef6386e81c9679
sagardss-tml <sagar.d@tatamotors.com>
2023-12-13 10:47:33 +0530

    book controller end points fix - Sagar

--- src/main/kotlin/com/sagardhone/bo_ok/BookController.kt
+++ src/main/kotlin/com/sagardhone/bo_ok/BookController.kt
@@ -8,16 +8,16 @@ class BookController(var bookService: BookService) {
     @GetMapping
     fun getAllBooks(): List<Bo_ok> = bookService.getAllBooks()
 
-    @GetMapping("/{studentId}")
+    @GetMapping("/{bookId}")
     fun getBookById(@PathVariable bookId: String)= bookService.getBookById(bookId)
 
     @PostMapping()
     fun addBook(@RequestBody book: Bo_ok): Bo_ok = bookService.addBook(book)
 
-    @PutMapping("/{studentId}")
+    @PutMapping("/{bookId}")
     fun updateBookDetails(@PathVariable bookId: String, @RequestBody book: Bo_ok) = bookService.updateBookDetailsById(bookId, book)
 
-    @DeleteMapping("/{studentId}")
+    @DeleteMapping("/{bookId}")
     fun removeBookDetailsById(@PathVariable bookId: String) = bookService.removeBookById(bookId)
 
     @DeleteMapping()
