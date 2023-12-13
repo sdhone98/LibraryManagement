e4820e012102a0ba41dca8c62c8a8e52da0eb089
sagardss-tml <sagar.d@tatamotors.com>
2023-12-13 19:35:14 +0530

    Book validation check added - Sagar

--- src/main/kotlin/com/sagardhone/bo_ok/BookController.kt
+++ src/main/kotlin/com/sagardhone/bo_ok/BookController.kt
@@ -1,4 +1,5 @@
 package com.sagardhone.library_management.bo_ok
+import com.sagardhone.bo_ok.validateBookObject
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,7 +13,12 @@ class BookController(var bookService: BookService) {
     fun getBookById(@PathVariable bookId: String)= bookService.getBookById(bookId)
 
     @PostMapping()
-    fun addBook(@RequestBody book: Bo_ok): Bo_ok = bookService.addBook(book)
+    fun addBook(@RequestBody book: Bo_ok):Bo_ok {
+
+        /* VALIDATE AND CHECK BOOK NAME CORRECT OR NOT */
+        validateBookObject(book)
+        return bookService.addBook(book)
+    }
 
     @PutMapping("/{bookId}")
     fun updateBookDetails(@PathVariable bookId: String, @RequestBody book: Bo_ok) = bookService.updateBookDetailsById(bookId, book)
