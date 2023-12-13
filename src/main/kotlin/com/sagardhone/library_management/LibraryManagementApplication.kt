6d4da71963963b62e9bbf9c8bb59578874865b11
sagardss-tml <sagar.d@tatamotors.com>
2023-12-13 19:38:14 +0530

    Custom Exception added - Sagar

--- src/main/kotlin/com/sagardhone/library_management/LibraryManagementApplication.kt
+++ src/main/kotlin/com/sagardhone/library_management/LibraryManagementApplication.kt
@@ -2,8 +2,10 @@ package com.sagardhone.library_management
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.sagardhone"])
 class LibraryManagementApplication
 
 fun main(args: Array<String>) {
