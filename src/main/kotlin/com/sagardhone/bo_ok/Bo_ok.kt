9c4ee31e11608dd9e14554528f6dd08eeb588cae
sagardss-tml <sagar.d@tatamotors.com>
2023-12-13 19:34:43 +0530

    Book constructor updated - Sagar

--- src/main/kotlin/com/sagardhone/bo_ok/Bo_ok.kt
+++ src/main/kotlin/com/sagardhone/bo_ok/Bo_ok.kt
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
 import org.hibernate.annotations.GenericGenerator
 import java.util.Date
 
@@ -22,6 +23,7 @@ data class Bo_ok (
     var id: String,
 
     @Column(name = "NAME", nullable = false, length = 300, unique = true)
+    @NotBlank(message = "name should not be empty.")
     var name: String,
 
     @Column(name="PAGE_COUNT", nullable = true)
@@ -34,5 +36,15 @@ data class Bo_ok (
     @Column(name ="PRICE", nullable = false)
     var price: Double,
 ){
-    constructor():this("","",null,null,0.0)
+
+    // Secondary constructor without id
+    constructor(
+        nameParam: String,
+        pageCountParam: Int?,
+        publicationDateParam: Date?,
+        priceParam: Double
+    ) : this("1", nameParam, pageCountParam, publicationDateParam, priceParam)
+
+    // Empty constructor for JPA
+    constructor() : this("", "", null, null, 0.0)
 }
