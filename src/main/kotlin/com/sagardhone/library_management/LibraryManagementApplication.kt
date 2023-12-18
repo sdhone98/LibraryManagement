f53353c18cc09e9173ed126c0892d4d1c206fdb6
sagardss-tml <sagar.d@tatamotors.com>
2023-12-18 10:38:48 +0530

    Entity sacn added - Sagar

--- src/main/kotlin/com/sagardhone/library_management/LibraryManagementApplication.kt
+++ src/main/kotlin/com/sagardhone/library_management/LibraryManagementApplication.kt
@@ -1,12 +1,17 @@
 package com.sagardhone.library_management
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.sagardhone"])
+@EntityScan(basePackages = ["com.sagardhone"])
+@EnableJpaRepositories(basePackages = ["com.sagardhone"])
 class LibraryManagementApplication
+
 
 fun main(args: Array<String>) {
 	runApplication<LibraryManagementApplication>(*args)
