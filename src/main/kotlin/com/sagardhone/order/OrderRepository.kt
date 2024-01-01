21e5ff746e58bd255a7f746d665bc0e94ea1c6ff
sagardss-tml <sagar.d@tatamotors.com>
2024-01-01 17:06:22 +0530

    Order Repository updated - sagar

--- src/main/kotlin/com/sagardhone/order/OrderRepository.kt
+++ src/main/kotlin/com/sagardhone/order/OrderRepository.kt
@@ -1,11 +1,27 @@
 package com.sagardhone.order
 
+import com.sagardhone.library_management.student.Student
+import com.sagardhone.textBook.TextBook
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface OrderRepository:JpaRepository<Order, String>{
-    @Query("SELECT o from Order o WHERE o.student = :studentId")
+    @Query("SELECT o from Order o WHERE o.student.id = :studentId")
     fun getAllOrdersByStudentId(studentId: String):List<Order>
+    @Query("SELECT COUNT(*) from Order o WHERE o.student.id = :studentId")
+    fun getCountOfOrdersByStudentId(studentId: String):Int
+
+    @Query("SELECT o FROM Order o WHERE o.student.id = :studentId AND o.textbook.id = :textBookId")
+    fun getOrderByStudentIdAndTextBookId(studentId: String, textBookId: String):Order?
+
+    @Query("SELECT o.textbook FROM Order o WHERE o.student.id = :studentId")
+    fun getTextBookDetailsByStudentId(studentId: String): List<TextBook>
+
+
+    @Query("SELECT o.student FROM Order o WHERE o.textbook.id = :textBookId")
+    fun getStudentDetailsByTextBookId(textBookId: String): List<Student>
+
+
 }
