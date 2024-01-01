9d29f10f680b8d73777fc437ab31c37590c220c6
sagardss-tml <sagar.d@tatamotors.com>
2024-01-01 17:05:07 +0530

    Order Entity updated - Sagar

--- src/main/kotlin/com/sagardhone/order/Order.kt
+++ src/main/kotlin/com/sagardhone/order/Order.kt
@@ -9,30 +9,31 @@ import org.hibernate.annotations.GenericGenerator
 import java.util.*
 
 @Entity
-@Table(name = "department")
+@Table(name = "book_order")
 class Order(
 
     @Id
     @GeneratedValue(generator = "order-custom-id", strategy = GenerationType.IDENTITY)
-    @GenericGenerator(name = "order-custom-id", strategy = "com.sagardhone.customIdGenerator.DepartmentCustomIdGenerator")
+    @GenericGenerator(name = "order-custom-id", strategy = "com.sagardhone.customIdGenerator.OrderCustomIdGenerator")
     @Column(name = "ID", nullable = false, updatable = false)
     var id: String,
 
-    @Column(name = "STUDENT_ID", nullable = false)
-    @OneToMany(cascade = [CascadeType.ALL])
+    @ManyToOne(cascade = [CascadeType.ALL], optional = false)
     @JsonManagedReference
     var student: Student,
 
-    @Column(name = "TEXT_BOOK_ID", nullable = false)
-    @OneToMany(cascade = [CascadeType.ALL])
+    @ManyToOne(cascade = [CascadeType.ALL], optional = false)
     @JsonManagedReference
     var textbook: TextBook,
-
 
     @Column(name ="ORDER_DATE_TIME", nullable = true)
     @JsonFormat(pattern="yyyy-MM-dd")
     var orderDateTime: Date,
-
-){
-    constructor():this("",Student(), TextBook(),Date())
+) {
+    constructor() : this("", Student(), TextBook(), Date())
+    constructor(
+        student: Student,
+        textbook: TextBook,
+        date: Date
+    ) : this("11", student, textbook, date)
 }
